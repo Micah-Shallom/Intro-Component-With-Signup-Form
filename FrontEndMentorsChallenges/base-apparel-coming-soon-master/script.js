4777f52c86dca7a33d0c32038a8905511a907ef7
Micah, Shallom <micahshallom@gmail.com>
2020-06-23 14:42:16 +0100

    added form validation

--- FrontEndMentorsChallenges/base-apparel-coming-soon-master/script.js
+++ FrontEndMentorsChallenges/base-apparel-coming-soon-master/script.js
@@ -1,19 +1,28 @@
-// const input=document.querySelector('.formInput');
-// const form=document.getElementById('form');
-// const small=document.querySelector('small');
-// const button=document.getElementById('btn');
+const formInput=document.querySelector('.formInput');
+const button=document.getElementById('btn');
+const small=document.querySelector('.small');
+const sign=document.querySelector('.sign')
 
-// button.addEventListener('click',()=>{
 
-//    checkEmail(input);
-   
-// })
+button.addEventListener('click',(e)=>{
+    e.preventDefault()
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(formInput.value) && formInput.value!==''){
+        showSuccess();
+    }else{
+        showError();
+}
+});
 
-// function checkEmail(x){
-//    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//    if(re.test(x.value)){
-//       input.classList.add('success');
-//    }else{
-//       small.classList='error';
-//    }
-// }
+function showError(){
+    formInput.value=''
+    formInput.focus();
+    const form=formInput.parentElement;
+    form.className='form error';
+    small.className='small error'
+}
+function showSuccess(){
+    small.className='small';
+    const form=formInput.parentElement;
+    form.className='form';
+}
